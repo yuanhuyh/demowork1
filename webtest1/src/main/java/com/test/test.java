77a3e8f4edcd3cbbeea2aec1b8d11ecd1a5547bb
yuanhuyh <873975766@qq.com>
2018-05-23 20:15:03 +0800

    2.0

---
+++ webtest1/src/main/java/com/test/test.java
@@ -0,0 +1,25 @@
+package com.test;
+
+public class test {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public test(String name) {
+        this.name = name;
+    }
+
+    public test() {
+    }
+
+    public void show(){
+        System.out.println("1");
+    }
+}
